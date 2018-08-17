195c01f04aca4490287e886e0d329b169a55bf46
max <maximpely@gmail.com>
2018-08-17 15:26:50 +0100

    added queue lock so the player queue dose not run out of players partway through getting them

--- trainer.py
+++ trainer.py
@@ -1,12 +1,17 @@
 from queue import Queue
 from random import shuffle, choice
-from threading import Thread, Event
+from threading import Thread, Event, Lock
 
 from board import Board
 from player import NNPlayer
 
 
-def trainer_thread(queue, thread_id, quit_event):
+class OddPopulationError(Exception):
+    def __init__(self):
+        super().__init__("please use an even population size so every bot is guaranteed a game each sub generation")
+
+
+def trainer_thread(queue, thread_id, quit_event, queue_lock):
     """
     A thread tha pulls two NNPlayers from a queue and plays a game with them, updates their elo and puts them in a
     separate queue
@@ -16,12 +21,16 @@ def trainer_thread(queue, thread_id, quit_event):
     :type thread_id: int
     :param quit_event: an event to tell the thread when to quit
     :type quit_event: Event
+    :param queue_lock: a lock to make sure that the last player is not taken between the two gets
     """
     print(f"Thread {thread_id} started")
     while not quit_event.is_set():
         while queue.qsize() > 1:
-            player_one = queue.get()
-            player_two = queue.get()
+            with queue_lock:
+                if queue.qsize() < 2:
+                    break
+                player_one = queue.get()
+                player_two = queue.get()
             board = Board(player_one, player_two)
             NNPlayer.update_elo(*board.play())
     print(f"Thread {thread_id} quitting")
@@ -71,13 +80,17 @@ def train(population_size, fraction_kept, generations, sub_generations, mutation
     :rtype: NNPlayer
     """
 
+    if population_size % 2 != 0:
+        raise OddPopulationError
+
     quit_event = Event()
+    queue_lock = Lock()
     players = [NNPlayer() for _ in range(population_size)]
     live_threads = []
     try:
         player_queue = Queue()
         for thread_id in range(threads):
-            thread = Thread(target=trainer_thread, args=(player_queue, thread_id, quit_event))
+            thread = Thread(target=trainer_thread, args=(player_queue, thread_id, quit_event, queue_lock))
             thread.setDaemon(True)
             thread.start()
             live_threads.append(thread)
@@ -101,7 +114,7 @@ def train(population_size, fraction_kept, generations, sub_generations, mutation
 if __name__ == '__main__':
     from player import HumanPlayer
 
-    best = train(population_size=100, fraction_kept=0.2, generations=10, sub_generations=10, mutation_rate=0.2,
+    best = train(population_size=100, fraction_kept=0.2, generations=10, sub_generations=2, mutation_rate=0.2,
                  threads=10)
     print("\n\nTraining Complete\n\n")
     print(f"best elo is {best.elo}\n\n")
